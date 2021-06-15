1fb7af6d02baf0440835a9f9d551f6711bb5e851
Godstime Osarobo <nosarobo@gmail.com>
2021-06-15 05:41:31 +0100

    fixed integration build

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,11 @@
+export const FirebaseConfig = {
+    apiKey: "AIzaSyD_3F9DvqD6NQAoeNv42Uqx_5s-e19s4Pc",
+    authDomain: "listen-and-lyrics.firebaseapp.com",
+    databaseURL: "https://listen-and-lyrics.firebaseio.com",
+    projectId: "listen-and-lyrics",
+    storageBucket: "listen-and-lyrics.appspot.com",
+    messagingSenderId: "506294338365",
+    appId: "1:506294338365:web:51b6ae54318302d683fb54",
+    measurementId: "G-ZR9QG0RMF6"
+
+}
